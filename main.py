8cc5ba116999806f80e408c076678a80dcfca37d
Gugo-le <rngustmd0719@naver.com>
2024-07-09 23:26:00 +0900

    운송비

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ import pandas as pd
 레인 = (2,2)
 
 print(제품, 대리점, 공장, 레인)
+
+# 대리점 - 공장 간 운송비용
+trans_cost = pd.DataFrame( ((j,k) for j in 대리점 for k in 공장), columns = ['대리점', '공장'])
+trans_cost['운송비'] = [1,2,3,1]
+trans_cost
